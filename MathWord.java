2bb82ab72767bcbe03b0fbcd58243e8c2b752ca8
Inam Ali <126110565+inam-chandio@users.noreply.github.com>
2023-04-02 22:57:27 +0500

    Add files via upload

--- MathWord.java
+++ MathWord.java
@@ -1,14 +1,36 @@
-public class MathWord {
-   public void Math(int x1,int x2, int y1,int y2){
+class MathWord{
 
-       System.out.println("The Distance between these Two Points is : "+ Math. sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2) ));
+ public double Rectangle(int w,int l){
+        System.out.println("The area of Rectangle is : "+ w*l);
+       return w*l;
    }
-
+   
+   public double Triangle(int b,int h) {
+        System.out.println("The area of Triangle is : " + (b* h)/2);
+        return (b*h)/2;
+    }
+	
+	  float PI=3.14f;
+    float Sphere= 4* PI;
+    public double Sphere(int r) {
+        System.out.println("The area of Sphere is : " + Sphere * r * r);
+        return 4 * PI * r * r;
+    }
+	
+	float Pi=3.14f;
+ public  void Circle(int r){
+        System.out.println("The Area of Circle is : " + Pi*r*r);
+ }
+   
     public static void main(String []args){
-		System.out.println("=======================================");
+
         MathWord obj=new MathWord();
-        obj.Math(4 ,-10,-2,-3);
+		System.out.println("-------------------------------");
+		obj.Circle(4);
+		obj.Sphere(5);
+        obj.Rectangle(  2,4);
+		obj.Triangle(6,3);
+		System.out.println("-------------------------------");
+		}
 		
-		System.out.println("=======================================");
-	}
-}
+		}
